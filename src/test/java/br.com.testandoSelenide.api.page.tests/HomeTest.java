0d7c99122358992fd3c9e036e16bc27101bd00c4
jeanheberth <jeanheberth19@gmail.com>
2024-10-09 19:10:36 -0300

    construindo

--- src/test/java/br.com.testandoSelenide.api.page.tests/HomeTest.java
+++ src/test/java/br.com.testandoSelenide.api.page.tests/HomeTest.java
@@ -1,55 +1,33 @@
 package br.com.testandoSelenide.api.page.tests;
 
+import com.codeborne.selenide.Selenide;
+import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
 
 
 public class HomeTest {
 
 
+    @BeforeMethod
+    public void setup() {
+        Selenide.open("https://automationexercise.com/");
+    }
+
+
     @Test
     public void testExample() {
-        Assert.assertTrue(true, "Test example passed");
+        $(By.xpath("//h1[contains(text(),'Automation Exercise')]")).shouldHave(text("Automation Exercise"));
+
     }
 
     @Test
     public void testExamples() {
-        Assert.assertTrue(true, "Test example passed");
-    }
-
-    @Test
-    public void testExamffspleaaas() {
-        Assert.assertTrue(true, "Test example passed");
-    }
-
-    @Test
-    public void testdfsExamples() {
-        Assert.assertTrue(true, "Test example passed");
-    }
-
-    @Test
-    public void testExampleaaas() {
-        Assert.assertTrue(true, "Test example passed");
-    }
-
-    @Test
-    public void testExadsdmpleaaas() {
-        Assert.assertTrue(true, "Test example passed");
-    }
-
-    @Test
-    public void testExdsdavgsdvampleaaas() {
-        Assert.assertTrue(true, "Test example passed");
-    }
-
-    @Test
-    public void testEfasdfdsaasdfsaxampleaaas() {
-        Assert.assertTrue(true, "Test example passed");
-    }
-
-    @Test
-    public void testEasfdsafxampleaaas() {
-        Assert.assertTrue(true, "Test example passed");
+        Assert.assertTrue(true, "Test examddddple passed");
     }
 }
 
