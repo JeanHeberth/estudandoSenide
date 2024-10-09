0356a821d103cb9e9c8516012d182bc0522fbb07
jeanheberth <jeanheberth19@gmail.com>
2024-10-09 17:07:09 -0300

    criando relatorio de teste

--- src/main/java/br/com/testandoSelenide/api/Main.java
+++
@@ -1,7 +0,0 @@
-package br.com.testandoSelenide.api;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/test/java/br.com.testandoSelenide.api.page.tests/HomeTest.java
@@ -0,0 +1,38 @@
+package br.com.testandoSelenide.api.page.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
+public class HomeTest {
+
+
+    @Test
+    public void testExample() {
+        Assert.assertTrue(true, "Test example passed");
+    }
+
+    @Test
+    public void testExamples() {
+        Assert.assertTrue(true, "Test example passed");
+    }
+
+    @Test
+    public void testExamffspleaaas() {
+        Assert.assertTrue(true, "Test example passed");
+    }
+
+    @Test
+    public void testdfsExamples() {
+        Assert.assertTrue(true, "Test example passed");
+    }
+
+    @Test
+    public void testExampleaaas() {
+        Assert.assertTrue(true, "Test example passed");
+    }
+}
+
+
+
+
